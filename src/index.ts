112ba903cf3ec930db5d48926cd9d24936b5ab51
iamtonmoy0 <tonmoy2code@gmail.com>
2024-01-02 01:31:49 +0600

    err fixed

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import * as fs from "fs";
 import * as path from "path";
 
-const routeSync = (app: any, pathName: string,baseRoute:string) => {
-  const routePath = path.join(__dirname, pathName);
+export const routeSync = (app: any, pathName: string, baseRoute: string) => {
+  fs.readdirSync(pathName).forEach((fileName) => {
+    const routeFilePath = path.join(pathName, fileName);
 
-  fs.readdirSync(routePath).forEach((fileName) => {
-    const routeFilePath = path.join(routePath, fileName);
-
-    if (path.extname(fileName) === ".js") {
-      const route = require(routeFilePath);
-      app.use(baseRoute, route);
-    }
+    const route = require(routeFilePath);
+    app.use(baseRoute, route);
   });
 };
-
-export default routeSync;
