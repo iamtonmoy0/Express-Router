f27a90ad40bd6bea7b357933544e6e3c81a2caad
iamtonmoy0 <tonmoy2code@gmail.com>
2024-01-02 01:12:10 +0600

    type error fixed

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import * as fs from "fs";
 import * as path from "path";
 
-const routeSync = (app: any, pathName: string) => {
-  const routePath = path.join(__dirname, `../routes/v1/${pathName}`);
+const routeSync = (app: any, pathName: string,baseRoute:string) => {
+  const routePath = path.join(__dirname, pathName);
 
   fs.readdirSync(routePath).forEach((fileName) => {
     const routeFilePath = path.join(routePath, fileName);
 
     if (path.extname(fileName) === ".js") {
       const route = require(routeFilePath);
-      app.use("/api/v1", route);
+      app.use(baseRoute, route);
     }
   });
 };
